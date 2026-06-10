c410bd9661d848ece71055336a97781a44a79c72
sumit-01pr <sumitprajapati3935@gmail.com>
2026-06-10 20:31:38 +0530

    Update API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,24 +1,20 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api",
 });
 
 api.interceptors.request.use(
   (config) => {
-    const token =
-      localStorage.getItem(
-        "token"
-      );
+    const token = localStorage.getItem("token");
 
     if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
-  }
+  },
+  (error) => Promise.reject(error)
 );
 
 export default api;
